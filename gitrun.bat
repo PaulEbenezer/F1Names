69d4b99934995fabf7e9c694c109e3b4fc3f6d7f
Ebenezer <paulebenezer.official@gmail.com>
2021-09-01 15:26:34 +0530

    first commit

---
+++ gitrun.bat
@@ -0,0 +1,17 @@
+cd C:\Users\Ebenezer Paul\Desktop\testFolder
+
+git init
+
+git status
+
+git add .
+
+git status
+
+git commit -m "first commit"
+
+git remote add origin https://github.com/PaulEbenezer/F1Names
+
+git push-u origin master
+
+pause
